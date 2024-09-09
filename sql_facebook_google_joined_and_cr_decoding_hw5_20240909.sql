6818faf1b339da1a2b3f86767e2c5cb5ced7b36f
Anton81951 <antonbugaichuk@gmail.com>
2024-09-09 13:25:56 +0300

    1

---
+++ sql_facebook_google_joined_and_cr_decoding_hw5_20240909.sql
@@ -0,0 +1,149 @@
+--MAIN TASK
+WITH facebook_google_joined_hw5_anton81951 AS (
+    SELECT 
+        ad_date, 
+        url_parameters,
+        COALESCE(spend, 0) AS spend,
+        COALESCE(impressions, 0) AS impressions,
+        COALESCE(reach, 0) AS reach,
+        COALESCE(clicks, 0) AS clicks,
+        COALESCE(leads, 0) AS leads,
+        COALESCE(value, 0) AS value
+    FROM 
+        facebook_ads_basic_daily
+    UNION ALL
+    SELECT 
+        ad_date, 
+        url_parameters,
+        COALESCE(spend, 0) AS spend,
+        COALESCE(impressions, 0) AS impressions,
+        COALESCE(reach, 0) AS reach,
+        COALESCE(clicks, 0) AS clicks,
+        COALESCE(leads, 0) AS leads,
+        COALESCE(value, 0) AS value
+    FROM 
+        google_ads_basic_daily
+)
+SELECT 
+    ad_date, 
+    CASE 
+        WHEN LOWER(decode_ukrainian(SUBSTRING(url_parameters, 'utm_campaign=([^&]*)'))) = 'nan' THEN NULL
+        ELSE LOWER(decode_ukrainian(SUBSTRING(url_parameters, 'utm_campaign=([^&]*)')))
+    END AS utm_campaign, 
+    SUM(spend) AS "total spent", 
+    SUM(impressions) AS "total impressions",
+    SUM(clicks) AS "total clicks",
+    SUM(value) AS "total value",
+    CASE
+        WHEN SUM(impressions) = 0 THEN 0
+        ELSE (SUM(clicks) * 100.0 / SUM(impressions))
+    END AS "CTR",
+    CASE
+        WHEN SUM(clicks) = 0 THEN 0
+        ELSE SUM(spend) / SUM(clicks)
+    END AS "CPC",
+    CASE 
+        WHEN SUM(impressions) = 0 THEN 0
+        ELSE ROUND(SUM(spend) / (SUM(impressions) * 1000.0), 2)
+    END AS "CPM",
+    CASE 
+        WHEN SUM(spend) = 0 THEN 0
+        ELSE ROUND((SUM(value) - SUM(spend)) * 100.0 / SUM(spend), 2)
+    END AS "ROMI (%)"
+FROM 
+    facebook_google_joined_hw5_anton81951
+GROUP BY 
+    ad_date, utm_campaign
+ORDER BY 
+    ad_date;
+
+--DECODING
+CREATE OR REPLACE FUNCTION decode_ukrainian (utm_campaign TEXT) RETURNS TEXT AS $$
+DECLARE
+    decoded_text TEXT;
+BEGIN
+    decoded_text := utm_campaign;
+
+    decoded_text := REPLACE(decoded_text, '%D0%B0', 'а'); -- а
+    decoded_text := REPLACE(decoded_text, '%D0%B1', 'б'); -- б
+    decoded_text := REPLACE(decoded_text, '%D0%B2', 'в'); -- в
+    decoded_text := REPLACE(decoded_text, '%D0%B3', 'г'); -- г
+    decoded_text := REPLACE(decoded_text, '%D0%B4', 'д'); -- д
+    decoded_text := REPLACE(decoded_text, '%D0%B5', 'е'); -- е
+    decoded_text := REPLACE(decoded_text, '%D1%91', 'є'); -- є
+    decoded_text := REPLACE(decoded_text, '%D0%B6', 'ж'); -- ж
+    decoded_text := REPLACE(decoded_text, '%D0%B7', 'з'); -- з
+    decoded_text := REPLACE(decoded_text, '%D0%B8', 'и'); -- и
+    decoded_text := REPLACE(decoded_text, '%D0%B9', 'ї'); -- ї
+    decoded_text := REPLACE(decoded_text, '%D0%BA', 'к'); -- к
+    decoded_text := REPLACE(decoded_text, '%D0%BB', 'л'); -- л
+    decoded_text := REPLACE(decoded_text, '%D0%BC', 'м'); -- м
+    decoded_text := REPLACE(decoded_text, '%D0%BD', 'н'); -- н
+    decoded_text := REPLACE(decoded_text, '%D0%BE', 'о'); -- о
+    decoded_text := REPLACE(decoded_text, '%D0%BF', 'п'); -- п
+    decoded_text := REPLACE(decoded_text, '%D1%80', 'р'); -- р
+    decoded_text := REPLACE(decoded_text, '%D1%81', 'с'); -- с
+    decoded_text := REPLACE(decoded_text, '%D1%82', 'т'); -- т
+    decoded_text := REPLACE(decoded_text, '%D1%83', 'у'); -- у
+    decoded_text := REPLACE(decoded_text, '%D1%84', 'ф'); -- ф
+    decoded_text := REPLACE(decoded_text, '%D1%85', 'х'); -- х
+    decoded_text := REPLACE(decoded_text, '%D1%86', 'ц'); -- ц
+    decoded_text := REPLACE(decoded_text, '%D1%87', 'ч'); -- ч
+    decoded_text := REPLACE(decoded_text, '%D1%88', 'ш'); -- ш
+    decoded_text := REPLACE(decoded_text, '%D1%89', 'щ'); -- щ
+    decoded_text := REPLACE(decoded_text, '%D1%8A', 'ь'); -- ь
+    decoded_text := REPLACE(decoded_text, '%D1%8B', 'и'); -- и
+    decoded_text := REPLACE(decoded_text, '%D1%8C', 'ь'); -- ь
+    decoded_text := REPLACE(decoded_text, '%D1%8D', 'е'); -- э
+    decoded_text := REPLACE(decoded_text, '%D1%8E', 'ю'); -- ю
+    decoded_text := REPLACE(decoded_text, '%D1%8F', 'я'); -- я
+
+    decoded_text := REPLACE(decoded_text, '%D0%90', 'А'); -- А
+    decoded_text := REPLACE(decoded_text, '%D0%91', 'Б'); -- Б
+    decoded_text := REPLACE(decoded_text, '%D0%92', 'В'); -- В
+    decoded_text := REPLACE(decoded_text, '%D0%93', 'Г'); -- Г
+    decoded_text := REPLACE(decoded_text, '%D0%94', 'Д'); -- Д
+    decoded_text := REPLACE(decoded_text, '%D0%95', 'Е'); -- Е
+    decoded_text := REPLACE(decoded_text, '%D0%81', 'Є'); -- Є
+    decoded_text := REPLACE(decoded_text, '%D0%96', 'Ж'); -- Ж
+    decoded_text := REPLACE(decoded_text, '%D0%97', 'З'); -- З
+    decoded_text := REPLACE(decoded_text, '%D0%98', 'И'); -- И
+    decoded_text := REPLACE(decoded_text, '%D0%99', 'Ї'); -- Ї
+    decoded_text := REPLACE(decoded_text, '%D0%9A', 'К'); -- К
+    decoded_text := REPLACE(decoded_text, '%D0%9B', 'Л'); -- Л
+    decoded_text := REPLACE(decoded_text, '%D0%9C', 'М'); -- М
+    decoded_text := REPLACE(decoded_text, '%D0%9D', 'Н'); -- Н
+    decoded_text := REPLACE(decoded_text, '%D0%9E', 'О'); -- О
+    decoded_text := REPLACE(decoded_text, '%D0%9F', 'П'); -- П
+    decoded_text := REPLACE(decoded_text, '%D0%A0', 'Р'); -- Р
+    decoded_text := REPLACE(decoded_text, '%D0%A1', 'С'); -- С
+    decoded_text := REPLACE(decoded_text, '%D0%A2', 'Т'); -- Т
+    decoded_text := REPLACE(decoded_text, '%D0%A3', 'У'); -- У
+    decoded_text := REPLACE(decoded_text, '%D0%A4', 'Ф'); -- Ф
+    decoded_text := REPLACE(decoded_text, '%D0%A5', 'Х'); -- Х
+    decoded_text := REPLACE(decoded_text, '%D0%A6', 'Ц'); -- Ц
+    decoded_text := REPLACE(decoded_text, '%D0%A7', 'Ч'); -- Ч
+    decoded_text := REPLACE(decoded_text, '%D0%A8', 'Ш'); -- Ш
+    decoded_text := REPLACE(decoded_text, '%D0%A9', 'Щ'); -- Щ
+    decoded_text := REPLACE(decoded_text, '%D0%AA', 'Ь'); -- Ь
+    decoded_text := REPLACE(decoded_text, '%D0%AB', 'И'); -- И
+    decoded_text := REPLACE(decoded_text, '%D0%AC', 'Ь'); -- Ь
+    decoded_text := REPLACE(decoded_text, '%D0%AD', 'Е'); -- Е
+    decoded_text := REPLACE(decoded_text, '%D0%AE', 'Ю'); -- Ю
+    decoded_text := REPLACE(decoded_text, '%D0%AF', 'Я'); -- Я
+
+    RETURN decoded_text;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+
+
+
+
+
+
+
+	
